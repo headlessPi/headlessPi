359e07a788964c016768471ee55e242cda892a5e
Ben Pirt <ben@pirt.co.uk>
2017-05-11 08:58:38 +0000

    Add fullscreen button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+// From https://github.com/neovov/Fullscreen-API-Polyfill
+!function(a){"use strict";function b(b,c){var d=a.createEvent("Event");d.initEvent(b,!0,!1),c.dispatchEvent(d)}function c(c){a[i.enabled]=a[e.enabled],a[i.element]=a[e.element],b(i.events.change,c.target)}function d(a){b(i.events.error,a.target)}var e,f,g=!0,h={w3:{enabled:"fullscreenEnabled",element:"fullscreenElement",request:"requestFullscreen",exit:"exitFullscreen",events:{change:"fullscreenchange",error:"fullscreenerror"}},webkit:{enabled:"webkitIsFullScreen",element:"webkitCurrentFullScreenElement",request:"webkitRequestFullScreen",exit:"webkitCancelFullScreen",events:{change:"webkitfullscreenchange",error:"webkitfullscreenerror"}},moz:{enabled:"mozFullScreenEnabled",element:"mozFullScreenElement",request:"mozRequestFullScreen",exit:"mozCancelFullScreen",events:{change:"mozfullscreenchange",error:"mozfullscreenerror"}},ms:{enabled:"msFullscreenEnabled",element:"msFullscreenElement",request:"msRequestFullscreen",exit:"msExitFullscreen",events:{change:"MSFullscreenChange",error:"MSFullscreenError"}}},i=h.w3;for(f in h)if(h[f].enabled in a){e=h[f];break}return!g||i.enabled in a||!e||(a.addEventListener(e.events.change,c,!1),a.addEventListener(e.events.error,d,!1),a[i.enabled]=a[e.enabled],a[i.element]=a[e.element],a[i.exit]=a[e.exit],Element.prototype[i.request]=function(){return this[e.request].apply(this,arguments)}),e}(document);
+
 function setupLinks(){
   var adminAppClickHandler = function(e){
     var path = e.target.pathname;
@@ -14,6 +17,17 @@ function setupLinks(){
   var adminLink = document.getElementById('adminLink');
   adminLink.addEventListener('click', function(e){
     document.querySelector('#admin').style.visibility = 'visible';
+    e.preventDefault();
+    return false;
+  });
+
+  var fullscreenLink = document.getElementById('fullscreen');
+  fullscreenLink.addEventListener('click', function(e){
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
     e.preventDefault();
     return false;
   });
