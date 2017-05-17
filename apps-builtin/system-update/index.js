d8d03756ce668bc92ac14fab16e8e4a3a2b41c53
Ben Pirt <ben@pirt.co.uk>
2017-05-17 10:54:56 +0000

    Add system update app

---
+++ apps-builtin/system-update/index.js
@@ -0,0 +1,33 @@
+var AppManager = require('../../lib/AppManager');
+var SystemUpdate = require('../../lib/SystemUpdate');
+var manager = new AppManager();
+var hb = require('handlebars');
+var fs = require('fs');
+const path = require('path');
+
+
+var subApp = function(){
+  this.setup = function(router){
+    router.get('/', function(req, res) {
+      fs.readFile(path.join(__dirname, './index.html'), 'utf-8', function(error, source){
+        var template = hb.compile(source);
+        res.send(template())
+      });
+    });
+
+    router.post('/update', function(req, res) {
+      var fullUpdate = !!req.query.full;
+      SystemUpdate.update(fullUpdate, (err) => {
+        if(err){
+          res.status(500).send(err);
+        }else{
+          res.sendStatus(200);
+        }
+      });
+    });
+  }
+  this.staticFolder = 'assets';
+  return this;
+}
+
+module.exports = subApp();
